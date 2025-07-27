c43495c0c178220380d90ffff306ac58cac54f55
anshumanmishhra1 <anshumanmishhra1@gmail.com>
2025-07-27 18:14:31 +0530

    Signup Stage-2 and login-1

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { configDotenv } from 'dotenv';
-import { connectDb } from './config/db.js';
-import jwt from 'jsonwebtoken'; // ✅ Correct import
-import User from './models/userSchema.js';
+import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import { connectDb } from "./config/db.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-configDotenv(); // ✅ Load env variables
+import User from "./models/userSchema.js";
+
+configDotenv();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -16,39 +18,58 @@ app.use(express.json());
 // Connect to DB
 connectDb();
 
-app.post('/user/signup', async (req, res) => {
-    try {
-        const user = req.body;
+app.post("/user/signup", async (req, res) => {
+  try {
+    const user = req.body;
 
-        if (!user.email || !user.name.firstName || !user.phone || !user.password || !user.age) {
-            return res.status(400).send('Something is missing');
-        }
-
-        //checking before signup that there is exist a user with the same email?
-       
-        
-        user.password = jwt.sign(
-            { data: 'foobar' }, 
-            process.env.SECRET_KEY, 
-            { expiresIn: '1h' }
-        );
-
-        const newUser = new User(user);
-        await newUser.save(); 
-
-
-        res.status(200).send("Signup Successfull");
-        console.log(user);
-
-    } catch (error) {
-        res.status(500).send(error.message); 
+    if (
+      !user.email ||
+      !user.name.firstName ||
+      !user.phone ||
+      !user.password ||
+      !user.age
+    ) {
+      return res.status(400).send("Something is missing");
     }
+
+    //checking before signup that there is exist a user with the same email?
+    const checkUser = await User.findOne({ email: user.email });
+    console.log(checkUser);
+    if (checkUser) {
+      res.status(400).send("pehle se hi hai bhai user");
+    }
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+
+    const newUser = new User(user);
+    await newUser.save();
+
+    res.status(200).send("Signup Successfull");
+    console.log(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.get('/login', (req, res) => {
-    res.status(200).send("hello user");
+app.get("/user/login", async (req, res) => {
+  try {
+    const user = req.body;
+    const userMail = await User.findOne({ email: user.email });
+    if (!userMail) {
+      res.status(400).send("user doesn't exist");
+    }
+
+    const token = jwt.sign({id:user._id},process.env.SECRET_KEY,{expiresIn:'30d'})
+
+    const isMatch = bcrypt.compare(password,user.password);
+    
+
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at ${PORT}`);
+  console.log(`Server is running at ${PORT}`);
 });
